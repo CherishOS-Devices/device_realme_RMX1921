dd738448bd8626f063708c3621f7688acf881445
Parixshit <parikshit.tunlr@slmail.me>
2022-07-10 17:40:12 +0530

    RMX1921: Cherish-ify!
    Signed-off-by: Parixshit <parikshit.tunlr@slmail.me>

---
+++ cherish_RMX1921.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Inherit some common CherishOS stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+CHERISH_VANILLA := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+
+# Official-ify
+CHERISH_BUILD_TYPE := OFFICIAL
+
+# Inherit from RMX1921 device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := Realme
+PRODUCT_DEVICE := RMX1921
+PRODUCT_MANUFACTURER := Realme
+PRODUCT_NAME := cherish_RMX1921
+PRODUCT_MODEL := Realme XT
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="RMX1921-user 11 RKQ1.201217.002 1626947099367 release-keys" \
+    PRODUCT_NAME="RMX1921"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := realme/RMX1921/RMX1921:11/RKQ1.201217.002/1626947099367:user/release-keys
+
+# Cherish Maintainer
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=αиυנвαяωαℓ
